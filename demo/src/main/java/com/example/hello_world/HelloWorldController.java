aa5d3c84fec5aed28cc1c831a2a76e7a0e6b41d9
NakshatraGowda <nakshatra.gowda.2000@gmail.com>
2025-12-19 18:42:01 +0530

    working hello world code

---
+++ demo/src/main/java/com/example/hello_world/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.hello_world;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello World from Spring Boot";
+    }
+}
+
